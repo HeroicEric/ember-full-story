97192b349c7789d3b411c157eea0067223ab29d2
Donald Wasserman <djwasserman@gmail.com>
2018-02-16 14:52:56 -0500

    added tests

--- tests/unit/services/full-story-test.js
+++ tests/unit/services/full-story-test.js
@@ -1,12 +1,48 @@
 import { moduleFor, test } from 'ember-qunit';
+import sinon from 'sinon';
+
 
 moduleFor('service:full-story', 'Unit | Service | full story', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+  beforeEach() {
+    window._fs_namespace = "FS"
+    window._fs_loaded = true;
+  },
+
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
+test('`invoke` calls the method named in full story', function(assert) {
   let service = this.subject();
-  assert.ok(service);
+  let idspy = sinon.spy();
+  window.FS = {
+    identify: idspy,
+  };
+
+  let id = '1234-5677'
+  service.invoke('identify', id);
+
+  assert.ok(idspy.calledOnce, 'It was called with the correct method name');
+  assert.ok(idspy.calledWith(id), 'It was called with correct args');
 });
+
+test('Method names calls underlying full story method', function(assert) {
+  let service = this.subject(),
+      userspy = sinon.spy();
+
+  window.FS = {
+    setUserVars: userspy
+  }
+
+  let props = {foo: "bsar"};
+  service.setUserVars(props);
+
+  assert.ok(userspy.calledOnce, 'setUserVars passed to fullstory');
+  assert.ok(userspy.calledWith(props), 'It was called with correct args');
+})
+
+test('compute properties should match underlying properties', function(assert) {
+  let service = this.subject();
+  assert.equal(service.get('fsLoaded'), window._fs_loaded)
+
+  window._fs_namespace = "ABCD"
+  assert.equal(service.get('fsNamespace'), window._fs_namespace);
+})
